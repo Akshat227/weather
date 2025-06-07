a04072646349a1b9dfa330a2ec049663b18b0093
Akshat <akshatkhare364@gmail.com>
2025-06-07 22:14:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import requests
+
+Api_key = "ffff534834c64c6ca28162222250706"
+Base_url ="http://api.weatherapi.com/v1/current.json"
+
+def weather(city):
+
+    url = f"{Base_url}?key={Api_key}&q={city}&aqi=no"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        city_name = data["location"]["name"]
+        region = data["location"]["region"]
+        country = data["location"]["country"]
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        feelslike = data["current"]["feelslike_c"]
+
+        print(f"\nWeather in {city_name}, {region}, {country}")
+        print(f"Temperature: {temp_c}°C")
+        print(f"Feels Like: {feelslike}°C")
+        print(f"Condition: {condition}\n")
+
+city = input("Enter the desiered city: ")
+weather(city)
+
